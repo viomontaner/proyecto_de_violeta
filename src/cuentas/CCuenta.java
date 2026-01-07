5cc91b3e974b2bcb00a61acfadf3d2e9262e88c2
viomontaner <viomontaner@gmail.com>
2026-01-07 21:23:20 +0100

    Ejercicio Refactorización, Javadoc y Git
    (Asignatura entornos de desarollo-Violeta Montaner)

---
+++ src/cuentas/CCuenta.java
@@ -0,0 +1,81 @@
+package cuentas;
+public class CCuenta {
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	public void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
+
+	public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+
+    public double estado()
+    {
+        return saldo;
+    }
+    /**
+     * Método para ingresar cantidades en la cuenta. Modifica el saldo.
+     * @param cantidad La cantidad a ingresar
+     * @throws Exception Falla si la cantidad es negativa
+     */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+    /**
+     * Método para retirar cantidades de la cuenta. Modifica el saldo.
+     * @param cantidad La cantidad a retirar
+     * @throws Exception Falla si la cantidad es negativa o si no hay saldo suficiente
+     */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        saldo = saldo - cantidad;
+    }
+}
